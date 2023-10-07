c12a5391cc0640101d20b31ef4f43efe80d8c576
Paulo <pvalvesgv@gmail.com>
2023-10-06 21:00:30 -0300

    Corrigindo LintWarnings

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -2,32 +2,23 @@
 
 namespace Chuva\Php\WebScrapping;
 
-use OpenSpout\Writer\Common\Creator\WriterEntityFactory;
 use OpenSpout\Writer\Common\Creator\Style\StyleBuilder;
-
-
+use OpenSpout\Writer\Common\Creator\WriterEntityFactory;
 /**
  * Runner for the Webscrapping exercice.
  */
 class Main {
-
   /**
    * Main runner, instantiates a Scrapper and runs.
    */
   public static function run(): void {
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
-
     $data = (new Scrapper())->scrap($dom);
-
-
-
     // Write your logic to save the output file bellow.
     $path = __DIR__ . '/../../assets/modelModf.xlsx';
     $writer = WriterEntityFactory::createXLSXWriter();
     $writer->openToFile($path);
-
-
     $writer->addRow(WriterEntityFactory::createRowFromArray(["Id", "Title", "Type", "Author 1", "Author 1 Institution",
        "Author 2", "Author 2 Institution", "Author 3",
         "Author 3 Institution", "Author 4", "Author 4 Institution",
@@ -37,23 +28,18 @@ class Main {
       ->setShouldWrapText()
       ->setShouldShrinkToFit()
       ->build()));
-
-
     $cells = [];
     foreach($data as $paper){
       $row = [];
       $row[] = $paper->id;
       $row[] = $paper->title;
       $row[] = $paper->type;
-      
       foreach($paper->authors as $author){
         $row[] = $author->name;
         $row[] = $author->institution;
       }
-      
       $cells[] = $row;
     }
-
     foreach($cells as $cell){
       $rowFromValues = WriterEntityFactory::createRowFromArray($cell,(new StyleBuilder())
       ->setFontSize(10)
@@ -61,7 +47,6 @@ class Main {
       ->build());
       $writer->addRow($rowFromValues);
     }
-
     $writer->close();
    }
 
--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -4,32 +4,23 @@ namespace Chuva\Php\WebScrapping;
 
 use Chuva\Php\WebScrapping\Entity\Paper;
 use Chuva\Php\WebScrapping\Entity\Person;
-
 /**
  * Does the scrapping of a webpage.
  */
 class Scrapper {
-
   /**
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    
     $data = [];
-
     $elements = $dom->getElementsByTagName("a");
     $cards = [];
-    foreach($elements as $element){
-
-      if(str_contains($element->getAttribute("class"), "paper-card")){
+    foreach ($elements as $element) {
+      if (str_contains($element->getAttribute("class"), "paper-card")) {
         $cards[] = $element;
       }
-      
     }
-
-
     foreach($cards as $card){
-      
       $authors = [];
       foreach($card->firstElementChild->nextSibling->getElementsByTagName("span") as $element){
         $authors[] = new Person(
@@ -37,18 +28,14 @@ class Scrapper {
           $element->getAttribute("title")
         );
       }
-
-
       $data[] = new Paper(
         $card->lastElementChild->lastElementChild->textContent,
         $card->firstElementChild->textContent,
         $card->lastElementChild->firstElementChild->textContent,
         $authors
       );
-
     }
       return $data;
-    }
+  }
+
 }
-
-
